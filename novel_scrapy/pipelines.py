7acbf6c9eaf2bc37374b95cc9a94a8edb12f1117
quuee <naageo@163.com>
2022-04-28 18:29:19 +0800

    up

---
+++ novel_scrapy/pipelines.py
@@ -0,0 +1,45 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+import os
+# from itemadapter import ItemAdapter
+
+# from novel_scrapy.items import NovelScrapyItem
+import pandas as pd
+
+
+class NovelScrapyPipeline:
+
+    def __init__(self):
+        self.book = pd.DataFrame(
+            columns=["sort", "title", "content"], index=[0])
+
+    def process_item(self, item, spider):
+        item_dict = {
+            "sort": item["sort"], "title": item["title"], "content": item["content"]}
+        book_new = pd.DataFrame(item_dict, index=[0])
+        self.book = pd.concat([self.book, book_new], axis=0, ignore_index=True)
+        return item
+
+    def close_spider(self, spider):
+        store_file = os.path.dirname(__file__) + "/temp.txt"
+        self.file = open(store_file, "a", encoding="utf-8")
+        # 网上看到用pandas排序
+        self.book = self.book.sort_values("sort", ascending=True)
+
+        title_list = list(self.book["title"])
+        content_list = list(self.book["content"])
+        for i in range(len(title_list)):
+            print(type(title_list[i]))
+            print(title_list[i])
+            self.file.write(str(title_list[i])+"\n")
+            # \r对应的ASCII码为：0xd，\n对应的ASCII码为：0xa
+            self.file.write(str(content_list[i]).replace(
+                "\xa0", "").replace("\r", "\n")+"\n")
+
+        self.file.close()
+
---
+++ novel_scrapy/spiders/novel_spider.py
@@ -0,0 +1,37 @@
+
+import scrapy
+
+from novel_scrapy.items import NovelScrapyItem
+
+
+class NovelSpider(scrapy.Spider):
+    name = "novel"
+    start_urls = ["https://www.81zw.com/book/45501/"]
+
+    def parse(self, response):
+        # print(type(response))
+        el_list = response.xpath("//div[@id='list']/dl/dd/a")
+        # print(type(el_list))
+        for el in el_list:
+            # print(el)
+            title = el.xpath("./text()").extract()
+            # print(type(title))
+            href = el.xpath("./@href").extract()
+            # print(type(href))
+            print(title[0]+" -- "+href[0])
+            if href is not None:
+                yield response.follow(href[0], callback=self.parse_chapter)
+
+
+    def parse_chapter(self, response):
+        title = response.xpath("//div[@class='bookname']/h1/text()").get()
+        txt = response.xpath("//div[@id='content']/text()").extract()
+        novel = NovelScrapyItem()
+
+        # https://www.81zw.com/book/45501/20758145.html
+        url = response.request.url
+        url = url[url.rindex("/")+1:url.rindex(".")]
+        novel["sort"] = int(url)
+        novel["title"] = title
+        novel["content"] = "\n".join(txt)
+        yield novel
